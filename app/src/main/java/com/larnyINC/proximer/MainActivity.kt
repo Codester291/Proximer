d01d404c95c82cb31c2e07acc807bc72a4d68812
Rextless OX <codester@github.com>
2019-02-27 11:47:12 +0000

    latest

--- app/src/main/java/com/larnyINC/proximer/MainActivity.kt
+++ app/src/main/java/com/larnyINC/proximer/MainActivity.kt
@@ -3,13 +3,27 @@ package com.larnyINC.proximer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.facebook.CallbackManager
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    var database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference("message")
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 
 
